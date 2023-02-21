135edcfd734afc0654a122200408d2ba8c2eb306
Hector Miguel Salazar Doroteo <hector_salazar12@hotmail.com>
2023-02-21 08:45:22 -0600

    test environment elastic beanstalk

--- application.py
+++ application.py
@@ -106,13 +106,13 @@ welcome = """
 </head>
 <body id="sample">
   <div class="textColumn">
-    <h1>Congratulations</h1>
-    <p>Your first AWS Elastic Beanstalk Python Application is now running on your own dedicated environment in the AWS Cloud</p>
+    <h1>Congratulations Salaxer</h1>
+    <p>Your first AWS Elastic Beanstalk Python Application is now running on your own dedicated environment in the AWS Cloud called Elastic beanstalk</p>
     <p>This environment is launched with Elastic Beanstalk Python Platform</p>
   </div>
   
   <div class="linksColumn"> 
-    <h2>What's Next?</h2>
+    <h2>What's Next?...</h2>
     <ul>
     <li><a href="http://docs.amazonwebservices.com/elasticbeanstalk/latest/dg/">AWS Elastic Beanstalk overview</a></li>
     <li><a href="http://docs.amazonwebservices.com/elasticbeanstalk/latest/dg/index.html?concepts.html">AWS Elastic Beanstalk concepts</a></li>
